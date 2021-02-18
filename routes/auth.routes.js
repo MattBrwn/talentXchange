bc60f1216b5dc24b1b08f06fa4460d6807136fb6
christiangerbig <chr_gerbig@web.de>
2021-02-18 12:52:39 +0100

    commit before merge

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,148 +1,174 @@
 const router = require("express").Router();
-const bcrypt = require('bcryptjs');
-const UserModel = require('../models/User.model')
+const bcrypt = require("bcryptjs");
+const UserModel = require("../models/User.model")
 
-/* GET signin page */
-router.get("/login", (req, res, next) => {
-    // Shows the sign in form to the user
-    res.render('auth/login.hbs')
-});
+// Middleware to protect routes
+// Check if a user is logged in
+function checkLoggedInUser(req, res, next) {
+  if (req.session.userData) {
+    next();
+  }
+  else {
+    res.redirect(
+      "/login"
+    );
+  }
+}
 
 /* GET signup page */
-router.get("/signup", (req, res, next) => {
+router.get(
+  "/signup",
+  (req, res, next) => {
     // Shows the sign up form to the user
-    res.render('auth/signup.hbs')
-});
+    res.render(
+      "auth/signup.hbs"
+    );
+  }
+);
 
 // Handle POST requests to /signup
 // when the user submits the data in the sign up form, it will come here
-router.post("/signup", (req, res, next) => {
+router.post(
+  "/signup",
+  (req, res, next) => {
     // we use req.body to grab data from the input form
-     const {name, email, password} = req.body
-    //console.log(req.body) // check if this is an empty object
-    // if not use the length 
-
-
-    //validate first
+    const { name, email, password } = req.body;
+    // validate first
     // checking if the user has entered all three fields
-    // we're missing one important step here
+    // we"re missing one important step here
     if (!name.length || !email.length || !password.length) {
-        res.render('auth/signup', {msg: 'Please enter all fields'})
-        return;
+      res.render(
+        "auth/signup.hbs",
+        { msg: "Please enter all fields" }
+      );
+      return;
     }
-
     // validate if the user has entered email in the right format ( @ , .)
-     // regex that validates an email in javascript
-     let re = /\S+@\S+\.\S+/;
-     if (!re.test(email)) {
-        res.render('auth/signup', {msg: 'Email not in valid format'})
-        return;
-     }
-
-     //validate password (special character, some numbers, min 6 length)
-     
-    //  let regexPass = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
-    //  if (!regexPass.test(password)) {
-    //     res.render('auth/signup', {msg: 'Password needs to have special chanracters, some numbers and be 6 characters aatleast'})
+    // regex that validates an email in javascript
+    let re = /\S+@\S+\.\S+/;
+    if (!re.test(email)) {
+      res.render(
+        "auth/signup.hbs",
+        { msg: "Email not in valid format" }
+      );
+      return;
+    }
+    // validate password (special character, some numbers, min 6 length)
+    // let regexPass = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+    // if (!regexPass.test(password)) {
+    //     res.render(
+    //         "auth/signup.hbs", 
+    //         {msg: "Password needs to have special chanracters, some numbers and be 6 characters aatleast"}
+    //     );
     //     return;
-    //  }
+    // }
+    // creating a salt 
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+    // Create user
+    let user = {
+      name,
+      email,
+      password: hash
+    }
+    UserModel.create(user)
+      .then(
+        (user) => {
+          req.session.userData = user
+          res.redirect(
+            "/ownprofile"
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          next(err);
+        }
+      );
+  }
+);
 
-  
 
-     // NOTE: We have used the Sync methods here. 
-     // creating a salt 
-     let salt = bcrypt.genSaltSync(10);
-     let hash = bcrypt.hashSync(password, salt);
-     UserModel.create({name, email, password: hash})
-        .then((user) => {
-            // req.session.userData = user
-            res.redirect('/ownprofile')
-        })
-        .catch((err) => {
-            next(err)
-        })
-});
+/* GET signin page */
+router.get(
+  "/login",
+  (req, res, next) => {
+    // Shows the sign in form to the user
+    res.render("auth/login.hbs");
+  }
+);
 
-// handle post requests when the user submits something in the sign in form
-router.post("/login", (req, res, next) => {
-    const {email, password} = req.body
-
+// handle POST requests when the user submits something in the sign in form
+router.post(
+  "/login",
+  (req, res, next) => {
+    const { email, password } = req.body;
     // implement the same set of validations as you did in signup as well
-    // NOTE: We have used the Async method here. Its just to show how it works
-    UserModel.findOne({email: email})
-        .then((result) => {
-            // if user exists
-            if (result) {
-                //check if the entered password matches with that in the DB
-                bcrypt.compare(password, result.password)
-                    .then((isMatching) => {
-                        if (isMatching) {
-                            // when the user successfully signs up
-                             req.session.userData = result
-                            //req.session.areyoutired = false
-                            //console.log(result)
-                            res.redirect('/overview')
-                        }
-                        else {
-                            // when passwords don't match
-                            res.render('/login.hbs', {msg: 'Passwords dont match'})
-                        }
-                    })
-            }
-            else {
-                // when the user signs in with an email that does not exits
-                res.render('auth/login.hbs', {msg: 'Email does not exist'})
-            }
-        })
-        .catch((err) => {
-            next(err)
-        })
-   
-});
+    UserModel.findOne({ email: email })
+      .then(
+        (result) => {
+          // if user exists
+          if (result) {
+            //check if the entered password matches with that in the DB
+            bcrypt.compare(password, result.password)
+              .then(
+                (isMatching) => {
+                  if (isMatching) {
+                    // when the user successfully signs up
+                    req.session.userData = result;
+                    //req.session.areyoutired = false
+                    //console.log(result)
+                    res.redirect(
+                      "/overview"
+                    );
+                  }
+                  else {
+                    // when passwords don"t match
+                    res.render("auth/login.hbs", { msg: "Passwords dont match" });
+                  }
+                }
+              );
+          }
+          else {
+            // when the user signs in with an email that does not exits
+            res.render(
+              "auth/login.hbs",
+              { msg: "Email does not exist" }
+            );
+          }
+        }
+      )
+      .catch(
+        (err) => {
+          next(err);
+        }
+      );
+  }
+);
 
-
-// GET request to handle /profile
-
-//Middleware to protect routes
-const checkLoggedInUser = (req, res, next) => {
-     if (req.session.userData) {
-        next()
-     }
-     else {
-         res.redirect('/login')
-     }
-     
-}
-
-router.get('/ownprofile', checkLoggedInUser, (req, res, next) => {
+// GET request to handle /ownprofile
+router.get(
+  "/ownprofile",
+  checkLoggedInUser,
+  (req, res, next) => {
     let email = req.session.userData.email;
-    let name = req.session.userData.name
-    res.render('ownprofile.hbs', {email} )
-})
+    let name = req.session.userData.name; /// ggf. überflüssig !!!!
+    res.render(
+      "ownprofile.hbs",
+      { email }
+    );
+  }
+);
 
-
-//router.get(path, callback,callback,callback,callback,callback)
-router.get('/logout', (req, res) => {
-    req.session.destroy()
-    res.redirect('/')
-})
-
-
-
-
-
+// handle logout
+router.get(
+  "/logout",
+  (req, res) => {
+    req.session.destroy();
+    res.redirect(
+      "/"
+    );
+  }
+);
 
 module.exports = router;
-
-
-
-// {"cookie":
-//     {"originalMaxAge":86400000,
-//     "expires":"2021-02-10T14:57:09.403Z",
-//     "httpOnly":true,
-//     "path":"/"},
-//  "userData":{
-//       "_id":"602269a49bbe4d24c814eba3",
-//       "name":"manish",
-//       "email":"a@a.com",
-//       "password":"$2a$10$Ft8J7o5v7ntc1hMJN/DIfe94l7VXrugk9ZraQyB6RYpMDfKOXq6I6","__v":0}}
--- routes/index.js
+++ routes/index.js
@@ -2,140 +2,227 @@ const RequestModel = require("../models/Request.model");
 const UserModel = require("../models/User.model");
 
 const router = require("express").Router();
-let allTalents = ['private tutoring', 'removal', 'gardening', 'household/housekeeping', 'office'];
 
 /* GET HOME PAGE */
-router.get("/", (req, res, next) => {
-  res.render("index"), {title: "Welcome Users on talentXchange's Website"};
-});
-
-
+router.get(
+  "/",
+  (req, res, next) => {
+    res.render(
+      "index.hbs",
+      {
+        title: "Welcome Users on talentXchange Website"
+      }
+    );
+  }
+);
 
 /* GET OWN PROFILE PAGE */
-router.get("/ownprofile/", (req, res, next) => {
-  //edit/save profileƒ
-  //let name = req.params.name
-  let userid = req.session.userData._id
-  UserModel.findById(userid) 
-    .then((user) => {
-      console.log(user.talents)
-      let name = user.name // what is when talents is not defined
-      let talents = user.talents // what is when talents is not defined
-      let description = user.description // what is when talents is not defined
-      res.render('ownprofile.hbs', {name, talents, description})
-    })
-    .catch((err) => connsole.log(err)); 
-});
-
+router.get(
+  "/ownprofile/",
+  (req, res, next) => {
+    // edit/save profile
+    // let name = req.params.name
+    let userId = req.session.userData._id;
+    UserModel.findById(userId)
+      .then(
+        (user) => {
+          let name = user.name; // what is when talents is not defined
+          let talents = user.talents; // what is when talents is not defined
+          let description = user.description; // what is when talents is not defined
+          res.render(
+            "ownprofile.hbs",
+            { name, talents, description }
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+);
 
 /* POST OWN PROFILE PAGE */
-router.post("/ownprofile/", (req, res, next) => {
-  //edit/save profile
-  //let name = req.params.name
-  const {talents, description} = req.body
-  let userid = req.session.userData._id
-  // console.log(description);
-
-  UserModel.findByIdAndUpdate(userid, {talents, description})
-  .then((user) => {
-    res.redirect('overview')
-  })
-  // UserModel.findByIdAndupdate({name, email, password: hash})
-  .catch((err) => connsole.log(err)); 
-  
-});
-
-
+router.post(
+  "/ownprofile/",
+  (req, res, next) => {
+    // let name = req.params.name ggf. überflüssig !!!!!
+    let userId = req.session.userData._id;
+    const { talents, description } = req.body;
+    let userRequirements = {
+      talents,
+      description
+    }
+    UserModel.findByIdAndUpdate(userId, userRequirements)
+      .then(
+        (user) => {
+          res.redirect(
+            "/overview"
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+);
 
 /* GET USER PROFILE PAGE */
-router.get("/userprofile/:id", (req, res, next) => {
-  //user profile
-const {id} = req.params
-UserModel.findById(id)
-  .then((user) => {
-    res.render('userprofile.hbs', {user})
-  })
-  .catch((err) => connsole.log(err)); 
-});
+router.get(
+  "/userprofile/:id",
+  (req, res, next) => {
+    const { id } = req.params;
+    UserModel.findById(id)
+      .then(
+        (user) => {
+          res.render(
+            "userprofile.hbs",
+            { user }
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+);
 
-router.post("/userprofile/:id", (req, res, next) => {
-  const {id} = req.params
-  let userid = req.session.userData._id
-  const {message} = req.body
-
-
-  UserModel.findById(id)
-    .then((requester) => {
-      let talent = requester.talents// Gardening
-      let request = {
-        requested: id,
-        searcher: userid,
-        message: message,
-        requested_talent: talent
-      }
-
-        RequestModel.create(request)
-          .then(() => {
-            res.redirect('/overview')
-          })
-          .catch((err) => connsole.log(err)); 
-            })
-  
-});
-
-
-
+// POST userprofile page
+router.post(
+  "/userprofile/:id",
+  (req, res, next) => {
+    const { id } = req.params;
+    let userId = req.session.userData._id;
+    const { message } = req.body;
+    UserModel.findById(id)
+      .then(
+        (requester) => {
+          let talent = requester.talents;
+          let request = {
+            requested: id,
+            searcher: userId,
+            message: message,
+            requested_talent: talent
+          };
+          RequestModel.create(request)
+            .then(
+              () => {
+                res.redirect(
+                  "/overview"
+                );
+              }
+            )
+            .catch(
+              (err) => {
+                console.log(err);
+              }
+            );
+        }
+      )
+  }
+);
 
 /* GET OVERVIEW PAGE */
-router.get("/overview", (req, res) => {
-  //protected page 
-  let name = req.session.userData.name
-  // 1. Find all requests for the logged in user
-  // Grab from RequestModel
-  let userId = req.session.userData._id
+router.get(
+  "/overview",
+  (req, res) => {
+    let name = req.session.userData.name;
+    let talent = req.session.userData.talents;
+    // 1. Find all requests for the logged in user
+    // Grab from RequestModel
+    let userId = req.session.userData._id;
+    RequestModel.find({ searcher: userId })
+      .populate("requested")
+      .then(
+        (myRequests) => {
+          // results1 is all the users you as a user have requested for
+          RequestModel.find({ requested: userId })
+            .populate("searcher")
+            .then(
+              (foreignRequests) => {
+                // results 2 is all the users that reuested for you as a talent
+                res.render(
+                  "overview.hbs",
+                  {
+                    name,
+                    myRequests,
+                    foreignRequests,
+                    talent
+                  }
+                )
+              }
+            )
+            .catch(
+              (err) => {
+                console.log(err);
+              }
+            );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+);
 
-  RequestModel.find({searcher: userId })
-    .populate('requested')
-    .then((myRequests) => {
-      // results1 is all the users you as a user have requested for
-      RequestModel.find({requested: userId })
-        .populate('searcher')
-        .then((foreignRequests) => {
-            // results 2 is all the users that reuested for you as a talent
-              res.render('overview.hbs', {name, myRequests, foreignRequests})
-        })
-        .catch((err) => connsole.log(err)); 
-    })
-
-  // console.log(name)
-  
-});
-
+/* GET DELETE PAGE */
+router.get(
+  "/delete",
+  (req, res, next) => {
+    let userId = req.session.userData._id;
+    UserModel.findByIdAndDelete(userId)
+      .then(
+        (user) => {
+          res.redirect(
+            "/"
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+);
 
 /* GET RESULTS PAGE */
-router.get("/results/:talent", (req, res, next) => {
-  //protected page
-  let userid = req.session.userData._id
-  let searchtalent = req.params.talent
-  UserModel.find()
-    .then((user) => {
-      let result = []
-      for (let i=0; i<user.length; i++) {
-        let talent = user[i].talents
-        if (talent === searchtalent) {
-          result.push(user[i]);
-        } 
-      }      
-      console.log(result);
-      res.render('results.hbs', {result, searchtalent})
-    })
-    .catch((err) => console.log(err)); 
-});
-
-router.get("/logout", (req, res) => {
-  req.session.destroy()
-  res.redirect("/")
-});
-
+router.get(
+  "/results/:talent",
+  (req, res, next) => {
+    let userId = req.session.userData._id; // ggf. überflüssig !!!!!
+    let searchtalent = req.params.talent;
+    UserModel.find()
+      .then(
+        (user) => {
+          let result = [];
+          for (let i = 0; i < user.length; i++) {
+            let talent = user[i].talents;
+            if (talent === searchtalent) {
+              result.push(user[i]);
+            }
+          }
+          res.render(
+            "results.hbs",
+            {
+              result, searchtalent
+            }
+          );
+        }
+      )
+      .catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+);
 
 module.exports = router;
